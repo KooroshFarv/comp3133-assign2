ac7287f793d0ceaca738ae8272f5f950ea05c41f
kooroshFarv <farvardin.koorosh@gmail.com>
2026-01-31 16:45:55 -0500

    div test

--- test/calc.test.js
+++ test/calc.test.js
@@ -28,16 +28,28 @@ describe("Calc Tests", function () {
 
 
 
-    it('multiple pass: multiple(200, 12) should return 2400', function() {
-        const result = calc.mul(200, 12)
-        console.log('pass: multiple(200, 12) expected 2400 and got', result)
-        expect(result).to.equal(2400)
-    })
+    // it('multiple pass: multiple(200, 12) should return 2400', function() {
+    //     const result = calc.mul(200, 12)
+    //     console.log('pass: multiple(200, 12) expected 2400 and got', result)
+    //     expect(result).to.equal(2400)
+    // })
 
-    it('multiple fail: multiple(200, 12) should return 2250', function() {
-        const result = calc.mul(200, 12)
-        console.log('fail: multiple(200, 12) expected 2250, got', result)
-        expect(result).to.equal(2250)
-    })
+    // it('multiple fail: multiple(200, 12) should return 2250', function() {
+    //     const result = calc.mul(200, 12)
+    //     console.log('fail: multiple(200, 12) expected 2250, got', result)
+    //     expect(result).to.equal(2250)
+    // })
 
+
+    it('div pass : div(20, 2) should return 10', function() {
+        const result = calc.div(20, 2)
+        console.log('pass: div(20,2) expected 10 and got', result)
+        expect(result).to.equal(10)
+    } )
+
+    it('div fail: div(20,2) should return 5', function() {
+        const result = calc.div(20,2)
+        console.log('fail: fiv(20,2) expected 5, got', result)
+        expect(result).to.equal(5)
+    })
 })
